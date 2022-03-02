261e44d9d1990554ba4bf240de4c56ce2395f4fa
yurak-choi <nozo8026@gmail.com>
2022-03-02 23:27:53 +0900

    recoil tutorial Selectors

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import {useRecoilValue} from 'recoil';
-import {todoListState} from '../recoil';
+import {filteredTodoListState} from '../recoil';
 import TodoItemCreator from './TodoItemCreator';
 import TodoItem from './TodoItem';
+import TodoListStats from "./TodoListStats";
+import TodoListFilters from "./TodoListFilters";
 
 function TodoList() {
-  const todoList = useRecoilValue(todoListState);
+  const todoList = useRecoilValue(filteredTodoListState);
 
   return (
     <>
+      <TodoListStats />
+      <TodoListFilters />
       <TodoItemCreator />
       {todoList.map(todoItem => (
         <TodoItem key={todoItem.id} item={todoItem} />
